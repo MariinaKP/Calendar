8361da3600a18ef92a570764c430443856c67de3
Marina Pironeva <pironevamarina@gmail.com>
2023-03-06 18:10:38 +0200

    Updated icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,7 +1,16 @@
 import {TfiAngleLeft, TfiAngleRight} from "react-icons/tfi";
-import {BiPencil} from "react-icons/bi";
+import {BiError, BiPencil} from "react-icons/bi";
 import {AiFillStar} from "react-icons/ai";
 import {TiTick} from "react-icons/ti";
 import {GoSignOut} from "react-icons/go";
+import {GrFormClose} from "react-icons/gr";
 
-export const icons = { TfiAngleRight, TfiAngleLeft, BiPencil, AiFillStar, TiTick, GoSignOut};
+export const icons = {
+  TfiAngleRight,
+  TfiAngleLeft,
+  BiPencil,
+  AiFillStar,
+  TiTick,
+  GoSignOut,
+  GrFormClose,
+  BiError };

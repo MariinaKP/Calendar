dffae31e2347ca9fe704284e04309dcfbd5ce8f6
Marina Pironeva <pironevamarina@gmail.com>
2023-03-06 18:10:15 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import "./styles/main.scss";
 import {Calendar} from "./components/Calendar/Calendar";
 import {Login} from "./components/Login/Login";
 import {Register} from "./components/Register/Register";
+import {ThemeSelector} from "./components/ThemeSelector/ThemeSelector";
 import {useContext} from "react";
-import {AuthContext, AuthProvider} from "./AuthContext";
+import {AuthContext, AuthProvider} from "./context/AuthContext";
 import {auth} from "./firebase-config";
 import {icons} from "./assets/icons";
+import {ThemeContext} from "./context/ThemeContext";
+import {Auth} from "./components/Auth/Auth";
 
 
 function App() {
   const { currentUser, setCurrentUser } = useContext(AuthContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
   useEffect(() => {
-    const user = localStorage.getItem("currentUser");
-
-    if (user) {
-      setCurrentUser(user);
-    }
-  }, []);
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
 
   return (
-    <AuthProvider>
+    <>
+      <ThemeSelector/>
       <div className={'wrapper'}>
         <Calendar/>
       </div>
@@ -29,14 +30,14 @@ function App() {
         {currentUser &&
             <div className={'logged-in'}>
                 <p>{currentUser.email}</p>
-                <icons.GoSignOut onClick={() => auth.signOut()}/>
+                <icons.GoSignOut className={'sign-out'} onClick={() => auth.signOut()}/>
             </div>}
         {!currentUser && <div className={'log-in'}>
             <Register/>
             <Login/>
         </div>}
       </div>
-    </AuthProvider>
+    </>
   );
 }
 
